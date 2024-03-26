735686ca475010333724580af73e8ca8172e6ae6
NickB521 <nicolasblackson@gmail.com>
2024-03-26 14:04:58 -0400

    config added

--- src/main/java/com/nicolasblackson/expenseTracker/domain/Expense/controllers/ExpensesController.java
+++ src/main/java/com/nicolasblackson/expenseTracker/domain/Expense/controllers/ExpensesController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/expenses")
+@RequestMapping("/expense")
 public class ExpensesController {
 
     private ExpensesService expensesService;
